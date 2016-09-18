76c084c567ac565f8983bf91b416045d754f2dec
Mark Shuttleworth <mark@ubuntu.com>
2016-09-18 10:26:46 -0700

    Start and stop commands for the daemon

--- scripts/bin/snap-launch.py
+++ scripts/bin/snap-launch.py
@@ -13,12 +13,45 @@ Start or stop rqlited instances based on the configuration file:
 
 """
 from docopt import docopt
-import glob, os, sys, string
-import subprocess, signal
+import glob, os, sys, string, time
+import subprocess, signal, errno
 import shutil
 
 # TODO
 #  - extract snap paths from env
+
+def get_pid(pid_fn):
+    "Retrieve a pid from a pid_file"
+    try: pid = int(open(pid_fn).readline())
+    except ValueError:
+        return None
+    return pid
+
+def check_pid(pid):
+    """Check whether pid exists. Courtesy StackExchange #568271"""
+    if pid < 0:
+        return False
+    if pid == 0:
+        # According to "man 2 kill" PID 0 refers to every process
+        # in the process group of the calling process.
+        # On certain systems 0 is a valid PID but we have no way
+        # to know that in a portable fashion.
+        raise ValueError('invalid PID 0')
+    try:
+        os.kill(pid, 0)
+    except OSError as err:
+        if err.errno == errno.ESRCH:
+            # ESRCH == No such process
+            return False
+        elif err.errno == errno.EPERM:
+            # EPERM clearly means there's a process to deny access to
+            return True
+        else:
+            # According to "man 2 kill" possible error values are
+            # (EINVAL, EPERM, ESRCH)
+            raise
+    else:
+        return True
 
 # Retrieve a set of instances from the config file
 def get_instances(snap_dir, cfg_fn):
@@ -57,11 +90,31 @@ def spawn(instances, instance_dir):
             except FileExistsError:
                 print('rqlited: error: failed to create store for "'+name+'"')
                 continue
+        # Check if there is a pid file for a running instance
+        pid_fn = data_dir+'/pid'
+        if os.path.exists(pid_fn):
+            pid = get_pid(pid_fn)
+            if pid:
+                print('rqlite: warning: pid file for "'+name+'" exists.')
+                if check_pid(pid):
+                    print('rqlite: error: "'+name+'" running, skipped.')
+                    continue
         log = open(data_dir+'/log', 'a+')
-        pid = subprocess.Popen(['/snap/rqlite/current/bin/rqlited',
+        instance_proc = subprocess.Popen(['/snap/rqlite/current/bin/rqlited',
             '-http', port, '-raft', raft, data_dir],
-            stdout=log, stderr=subprocess.STDOUT).pid
-        pidfile = open(data_dir+'/pid', 'w+')
+            stdout=log, stderr=subprocess.STDOUT)
+        pid = instance_proc.pid
+        # we'll give it one second to start up and see if it's exited
+        # unhappily
+        time.sleep(1)
+        returncode = instance_proc.poll()
+        if returncode != None:
+            if returncode == 0:
+                print('rqlited: "'+name+'" exited immediately.')
+            else:
+                print('rqlited: error: "'+name+'" failed to start."')
+            continue
+        pidfile = open(pid_fn, 'w+')
         pidfile.write(str(pid))
         pidfile.close()
         count += 1
@@ -72,12 +125,14 @@ def spawn(instances, instance_dir):
 def shutdown(instance_dir):
     count = 0
     for pid_file in glob.glob(instance_dir+'/*/pid'):
-        count += 1
-        try: pid = int(open(pid_file).readline())
-        except ValueError:
+        instance = os.path.basename(pid_file[:-4])
+        pid = get_pid(pid_file)
+        if pid == None:
             print('rqlited: error: bad pidfile '+pid_file)
-            continue
-        try: os.kill(pid, signal.SIGHUP)
+        try:
+            os.kill(pid, signal.SIGHUP)
+            print('rqlite: stopped "'+instance+'"')
+            count += 1
         except ProcessLookupError:
             print('rqlited: error: '+pid_file+' pid not found')
         try: os.remove(pid_file)
